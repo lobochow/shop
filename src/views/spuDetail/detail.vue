02a891f3d6862617e5db38c39bfa8167a674ab8d
lobochow <10931858+lobochow@user.noreply.gitee.com>
2022-05-19 01:57:37 +0800

    商品详情页面完成

--- src/views/spuDetail/detail.vue
+++ src/views/spuDetail/detail.vue
@@ -25,7 +25,47 @@
                   <div class="imgWrap" ref="imgWrap"><img src="@/assets/images/spuImg/iphone.png" alt="zoomImg"></div>
               </div>
           </div>
-          <div class="rightArea"></div>
+          <div class="rightArea">
+              <h5>微星（MSI）万图师 GeForce RTX 2060 VENTUS 12G OC 超频版 电竞游戏设计专业电脑显卡</h5>
+              <div class="price">
+                    <span>京东价</span>
+                    <span>¥2699.00</span>
+                    <span>降价通知</span>
+                    <div>
+                        <p>累计评价</p>
+                        <p>10万+</p>
+                    </div>
+              </div>
+              <p class="weight">
+                  <span>重量</span>
+                  <span>1.16kg</span>
+              </p>
+              <div class="skuList">
+                  <span>选择型号</span>
+                  <ul>
+                      <li>
+                          <img src="@/assets/images/spuImg/iphone2.png" alt="sku">
+                          <span>123123</span>
+                      </li>
+                  </ul>
+              </div>
+              <div class="service">
+                  <span>精选服务</span>
+                  <ul>
+                      <li>
+                          <span>5年只换不修</span>
+                          <div class="list">
+                                  <input type="checkbox">
+                                  <span>5年只换不修</span>
+                          </div>
+                      </li>
+                  </ul>
+              </div>
+              <div class="addCart">
+                  <input type="number" value="1" min="1" max="10">
+                  <span>加入购物车</span>
+              </div>
+          </div>
       </div>
   </div>
 </template>
@@ -91,14 +131,14 @@ methods:{
             
             .leftArea{
                 width: 400px;
-                border: 1px solid black;
                 position: relative;
 
                 .showImg{
                     position: relative;
                     text-align: center;
                     width: 400px;
-                    height: 400px;
+                    height: 402px;
+                    border: 1px solid black;
                     img{
                         max-width: 400px;
                         max-height: 400px;
@@ -124,7 +164,6 @@ methods:{
                 }
 
                 .selectImg{
-                    border:rgb(71, 90, 142) 1px solid;
                     width: 400px;
                     height: 100px;
                     display: flex;
@@ -178,6 +217,7 @@ methods:{
 
                     left: 399px;
                     top: -1px;
+                    z-index: 10;
 
                     .imgWrap{
                         width: 400px;
@@ -199,7 +239,179 @@ methods:{
             .rightArea{
                 width: 550px;
                 height: 400px;
-                border: 1px solid black;
+
+                >:not(:nth-child(1)){
+                    margin-top: 10px;
+                }
+
+                h5{
+                    color:#666666;
+                }
+
+                .price{
+                    background-color: #F3F3F3;
+                    padding: 10px;
+                    >:nth-child(1){
+                        font-size: 10px;
+                        letter-spacing: 5px;
+                        color: #999999;
+                    }
+
+                    >:nth-child(2){
+                        margin-left: 10px;
+                        color: #E2231A;
+                    }
+
+                    >:nth-child(3){
+                        margin-left: 10px;
+                        font-size: 12px;
+                        color: #007BBD;
+                    }
+
+                    >:nth-child(4){
+                        border-left: solid 1px #CCC;
+                        margin: 0px 10px 0px 0px;
+                        float: right;
+                        font-size: 10px;
+                        p{
+                            width: 100%;
+                            text-align: center;
+                            margin: 0px 10px 0px 0px;
+                        }
+                    }
+                }
+
+                .weight{
+                    padding: 10px;
+                    *{
+                        font-size: 10px;
+                    }
+
+                    >:nth-child(1){
+                        color: #999999;
+                        letter-spacing: 20px;
+                    }
+                }
+
+                .skuList{
+                    padding: 10px;
+                    font-size: 10px;
+                    display: flex;
+                    align-items: center;
+                    >:nth-child(1){
+                        color: #999999;
+                        vertical-align: middle;
+                    }
+
+                    ul{
+                        margin: 0px;
+                        padding-left: 10px;
+                        list-style: none;
+                        display: flex;
+                        align-items: center;
+
+                        li{
+                            border: #CCCCCC 1px solid;
+                            img{
+                                max-width: 30px;
+                                max-height: 30px;
+                                vertical-align: middle;
+                            }
+
+                            span{
+                                margin: 0px 10px;
+                            }
+
+                            &:hover{
+                                border: #E2231A 1px solid;
+                            }
+                        }
+                    }
+                }
+
+                .service{
+                    padding: 10px;
+                    display: flex;
+
+
+                    >span{
+                        font-size: 10px;
+                        color: #999999;
+                    }
+
+                    ul{
+                        list-style: none;
+                        display: flex;
+                        li{
+                            font-size: 12px;
+                            position: relative;
+                            margin-left: 10px;
+
+                            >span{
+                                position: relative;
+                                z-index: 2;
+                                padding: 2px;
+                                border: 1px solid #ccc;
+
+                                &:hover{
+                                    border: 1px solid #E2231A;
+                                    border-bottom: 1px solid white;
+
+                                    ~.list{
+                                        visibility: visible;
+                                    }
+                                }
+                            }
+
+                            .list{
+                                position: absolute;
+                                padding: 5px;
+                                display: flex;
+                                margin-top: 2px;
+                                visibility: hidden;
+                                width: 200px;
+                                height: 30px;
+                                border: 1px solid #ccc;
+                                background-color: white;
+
+                                >input{
+                                    margin-top: 2px;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                .addCart{
+                    margin-left: 10px;
+
+                    >input{
+                        font-size: 32px;
+                        width: 60px;
+                        text-align: center;
+                        vertical-align: middle;
+                        border: 1px solid #ccc;
+
+                        &::-webkit-inner-spin-button,
+                        &::-webkit-outer-spin-button{
+                            opacity: 1;
+                        }
+
+                        &:focus{
+                            outline: none;
+                        }
+                    }
+
+                    >span{
+                        margin-left: 10px;
+                        display: inline-block;
+                        background-color: #E2231A;
+                        color: white;
+                        height: 40px;
+                        padding: 5px 15px;
+                        vertical-align: middle;
+                    }
+                }
             }
         }
     }
