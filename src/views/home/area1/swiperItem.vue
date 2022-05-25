8f6fe433ad771973491335d4269dd6b0c365a406
lobochow <10931858+lobochow@user.noreply.gitee.com>
2022-05-26 04:30:12 +0800

    添加变量控制动画

--- src/views/home/area1/swiperItem.vue
+++ src/views/home/area1/swiperItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="imgWrap" :style="itemStyle">
+    <div class="imgWrap" :style="itemStyle" @click="testtest">
         <slot></slot>
     </div>
 </template>
@@ -11,66 +11,63 @@ export default {
         return {
             itemStyle: {
                 transform: '',
-                transition: ''
+                transition: '',
+                visibility: 'hidden',
+                "--distrance": ''
             }
         }
     },
     methods: {
+        testtest(){
+            console.log(1);
+        },
         initItem(activedIndex, index) {
-            if (activedIndex != index) {
-                this.translateLeft();
+            if (activedIndex == index) {
+                this.itemStyle.visibility = 'visible';
             }
         },
         translateItem(flag, newIndex, oldIndex, index) {
             if (index == newIndex) {
                 if (flag == 'left') {
-                    this.onAnimation();
-                    this.translateCenter();
+                    // this.onAnimation();
+                    // this.translateCenter();
+                    this.$el.className = 'imgWrap ltc';
+                    this.itemStyle.visibility = 'visible';
                 }
                 if (flag == 'right') {
-                    this.offAnimation();
-                    this.translateRight();
-                    setTimeout(() => {
-                        this.onAnimation();
-                        this.translateCenter();
-                    }, 10);
+                    // this.offAnimation();
+                    // this.translateRight();
+                    // setTimeout(() => {
+                    //     this.onAnimation();
+                    //     this.translateCenter();
+                    // }, 10);
+                    this.$el.className = 'imgWrap rtc';
+                    this.itemStyle.visibility = 'visible';
                 }
             } else if (index == oldIndex) {
                 if (flag == 'left') {
-                    this.translateRight();
+                    this.$el.className = 'imgWrap ctr';
+                    //this.translateRight();
                 }
                 if (flag == 'right') {
-                    this.translateLeft();
+                    this.$el.className = 'imgWrap ctl';
+                    //this.translateLeft();
                 }
             } else {
-                this.offAnimation();
-                this.translateLeft();
+                this.$el.className = 'imgWrap';
+                this.itemStyle.visibility = 'hidden';
             }
-        },
-        translateLeft() {
-            this.itemStyle.transform = `translateX(${-this.$el.offsetWidth}px)`;
-        },
-        translateRight() {
-            this.itemStyle.transform = `translateX(${+this.$el.offsetWidth}px)`;
-        },
-        translateCenter() {
-            this.itemStyle.transform = `translateX(0px)`;
-        },
-        onAnimation() {
-            this.itemStyle.transition = '0.25s';
-        },
-        offAnimation() {
-            this.itemStyle.transition = 'none';
         }
     },
     mounted(){
-        this.itemStyle['--distrance'] = this.$el.offsetWidth;
+        this.itemStyle['--distrance'] = this.$el.offsetWidth + 'px';
     }
 }
 </script>
 
 <style lang="less" scoped>
 .imgWrap {
+    --animation-time: 0.25s;
     width: 580px;
     height: 470px;
     display: flex;
@@ -79,6 +76,58 @@ export default {
     > * {
         max-width: 580px;
         max-height: 470px;
+    }
+}
+
+.ltc{
+    animation: ltc var(--animation-time) forwards linear;
+}
+
+.ctl{
+    animation: ctl var(--animation-time) forwards linear;
+}
+
+@keyframes ltc{
+    from{
+        transform: translateX(calc(-1 * var(--distrance)));
+    }
+    to{
+        transform: translateX(0px);
+    }
+}
+
+@keyframes ctl{
+    from{
+        transform: translateX(0px);
+    }
+    to{
+        transform: translateX(calc(-1 * var(--distrance)));
+    }
+}
+
+.rtc{
+    animation: rtc var(--animation-time) forwards linear;
+}
+
+.ctr{
+    animation: ctr var(--animation-time) forwards linear;
+}
+
+@keyframes rtc{
+    from{
+        transform: translateX(var(--distrance));
+    }
+    to{
+        transform: translateX(0px);
+    }
+}
+
+@keyframes ctr{
+    from{
+        transform: translateX(0px);
+    }
+    to{
+        transform: translateX(var(--distrance));
     }
 }
 </style>
